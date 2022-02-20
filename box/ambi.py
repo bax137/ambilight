951c7cb4c340e6d38ca215ff449d4f7071076642
bax137 <bax137@ibax.org>
2022-02-20 10:44:17 +0100

    threading

--- box/ambi.py
+++ box/ambi.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 #import chardet
-from distutils import text_file
-import os,sys,time,logging
+import os,sys,time,logging,threading,requests,json
 import spidev as SPI
 sys.path.append("/home/pi/LCD_Module_code/RaspberryPi/python")
 from lib import LCD_1inch28
 from PIL import Image,ImageDraw,ImageFont
 import RPi.GPIO as GPIO
-import requests
-import json
 from datetime import datetime
 
+######################
+#       config       #
+######################
 rotation=-6
 hyperhdr_url = "http://192.168.1.134:8090/json-rpc/"
+fan_speed=90
 
 ######################
 #        pins        #
@@ -38,113 +39,163 @@ def dispBackgroung(image):
     draw.arc((3,3,237,237),0, 360, fill =(0,0,255))
     return draw
 
-def dispText(disp,text1, text2, color1 = "PURPLE", color2 = "YELLOW"):
+def dispText(disp,textH, textM, text, colorTime = "PURPLE", colorText = "YELLOW"):
     image = Image.new("RGB", (disp.width, disp.height), "BLACK")
     draw = dispBackgroung(image)
-    draw.text((51, 45), text1, fill = color1,font=Font3)
-    draw.text((50, 125), text2, fill = color2,font=Font4)
+    draw.text((51, 45), textH, fill = colorTime,font=Font3)
+    draw.text((121, 45), textM, fill = colorTime,font=Font3)
+    draw.text((50, 125), text, fill = colorText,font=Font4)
     disp.ShowImage(image.rotate(rotation))
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+class CurrentParams():
+    def __init__(self):
+        super(CurrentParams, self).__init__()
 
-######################
-#       fan up       #
-######################
-GPIO.setup(fan_PWM, GPIO.OUT)
-fan_pwm = GPIO.PWM(fan_PWM,25000)
-fan_pwm.start(90)
-#soft_pwm.ChangeDutyCycle(90)
+class Button(threading.Thread):
+    def __init__(self):
+        super(Button, self).__init__()
 
-######################
-#     init button    #
-######################
-GPIO.setup(but_OUT1, GPIO.OUT)
-GPIO.output(but_OUT1, True)
-GPIO.setup(but_OUT2, GPIO.OUT)
-GPIO.output(but_OUT2, True)
-GPIO.setup(but_IN,GPIO.IN)
-button_previous = 1
-button_current = 1
-brojac = 0
-flag_pressed = 0
-
-######################
-#     init screen    #
-######################
-
-logging.basicConfig(level=logging.DEBUG)
-Font1 = ImageFont.truetype("/home/pi/Font/Font01.ttf",25)
-Font2 = ImageFont.truetype("/home/pi/Font/Font01.ttf",35)
-Font3 = ImageFont.truetype("/home/pi/Font/Font02.ttf",65)
-Font4 = ImageFont.truetype("/home/pi/Font/Font02.ttf",35)
-
-
-# display with hardware SPI:
-''' Warning!!!Don't  creation of multiple displayer objects!!! '''
-disp = LCD_1inch28.LCD_1inch28(spi=SPI.SpiDev(disp_bus, disp_device),spi_freq=10000000,rst=disp_RST,dc=disp_DC,bl=disp_BL)
-# Initialize library.
-disp.Init()
-disp.clear()
-
-# initialize display
-dispText(disp,"..:..","Initialisaiton")
-
-while True:
-    button_current = GPIO.input(but_IN)
-    flag_pressed = button_previous + button_current
-
-    if (not(flag_pressed)):
-        brojac += 1
-    else:
+    def run(self):
+        button_previous = 1
+        button_current = 1
         brojac = 0
+        flag_pressed = 0
+        while True:
+            button_current = GPIO.input(but_IN)
+            flag_pressed = button_previous + button_current
 
-    if (button_current and (not button_previous)):
-        dispText(disp,"Shutdown","...........")
+            if (not(flag_pressed)):
+                brojac += 1
+            else:
+                brojac = 0
 
+            if (button_current and (not button_previous)):
+                dispText(disp,"Shutdown","...........")
+                print("Shutdown")
+                os.system("shutdown -h now")
+                GPIO.output(but_OUT1, True)
+                GPIO.output(but_OUT2, False)
 
-        print("Shutdown")
-        os.system("shutdown -h now")
-        GPIO.output(but_OUT1, True)
-        GPIO.output(but_OUT2, False)
+                disp.module_exit()
+                #fan_pwm.stop()
+                GPIO.cleanup()
+                break
 
-    if ((not flag_pressed) and  brojac >= 100):
-        dispText(disp,"Reboot","...........")
+            if ((not flag_pressed) and  brojac >= 100):
+                dispText(disp,"Reboot","...........")
 
-        print("Reboot")
-        os.system("Reboot -r now")
-        GPIO.output(but_OUT1, False)
-        break
+                print("Reboot")
+                os.system("Reboot -r now")
+                GPIO.output(but_OUT1, False)
+                disp.module_exit()
+                #fan_pwm.stop()
+                GPIO.cleanup()
+                break
 
-    button_previous = button_current
+            button_previous = button_current
 
-    #HYPERHDR
-    payload = json.dumps({
-        "command": "serverinfo",
-        "tan": 0
-    })
-    headers = {
-        'Content-Type': 'application/json'
-    }
+            time.sleep(0.03)
 
-    text = "HyperHDR OFF"
-    color = "RED"
-    try:
-        response = requests.request("POST", hyperhdr_url, headers=headers, data=payload)
-        json_resp= json.loads(response.text)
-        if (json_resp["success"] == True):
-            text="HyperHDR ON"
-            color="GREEN"
-    except:
-        text = "HyperHDR OFF"
-        color = "RED"
-    now = datetime.now()
-    current_time = now.strftime("%H:%M")
-    dispText(disp,current_time,text,color2=color)
+class Screen(threading.Thread):
+    def __init__(self):
+        super(Screen, self).__init__()
+        self.text = ""
+        self.text_color = "YELLOW"
+
+    def run(self):
+        step_sec=0
+        while True:
+            now = datetime.now()
+            M=now.strftime("%M")
+            H=""
+            if step_sec==1:
+                step_sec=0
+                H = now.strftime("%H:")
+            else:
+                step_sec=1
+                H = now.strftime("%H")
+            dispText(disp,H,M,self.text,colorText=self.text_color)
+            
+            time.sleep(0.5)
+
+class HyperHDR(threading.Thread):
+    def __init__(self):
+        super(HyperHDR, self).__init__()
+        
+    def run(self):
+        step_sec=0
+        while True:
+            #HYPERHDR
+            payload = json.dumps({
+                "command": "serverinfo",
+                "tan": 0
+            })
+            headers = {
+                'Content-Type': 'application/json'
+            }
+
+            try:
+                response = requests.request("POST", hyperhdr_url, headers=headers, data=payload)
+                json_resp= json.loads(response.text)
+                if (json_resp["success"] == True):
+                    screen.text="HyperHDR ON"
+                    screen.text_color="GREEN"
+            except:
+                screen.text = "HyperHDR OFF"
+                screen.text_color = "RED"
+            
+            time.sleep(1)
+
+if __name__ == "__main__":
+
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+
+    ######################
+    #       fan up       #
+    ######################
+    GPIO.setup(fan_PWM, GPIO.OUT)
+    fan_pwm = GPIO.PWM(fan_PWM,25000)
+    fan_pwm.start(fan_speed)
+    #soft_pwm.ChangeDutyCycle(90)
+
+    ######################
+    #     init button    #
+    ######################
+    GPIO.setup(but_OUT1, GPIO.OUT)
+    GPIO.output(but_OUT1, True)
+    GPIO.setup(but_OUT2, GPIO.OUT)
+    GPIO.output(but_OUT2, True)
+    GPIO.setup(but_IN,GPIO.IN)
+
+    ######################
+    #     init screen    #
+    ######################
+
+    logging.basicConfig(level=logging.DEBUG)
+    Font1 = ImageFont.truetype("/home/pi/Font/Font01.ttf",25)
+    Font2 = ImageFont.truetype("/home/pi/Font/Font01.ttf",35)
+    Font3 = ImageFont.truetype("/home/pi/Font/Font02.ttf",65)
+    Font4 = ImageFont.truetype("/home/pi/Font/Font02.ttf",35)
+
+    currentParams = CurrentParams()
+
+    # display with hardware SPI:
+    ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
+    disp = LCD_1inch28.LCD_1inch28(spi=SPI.SpiDev(disp_bus, disp_device),spi_freq=10000000,rst=disp_RST,dc=disp_DC,bl=disp_BL)
+    # Initialize library.
+    disp.Init()
+    disp.clear()
+
+    screen = Screen()
+    screen.start()
     
-    #time.sleep(0.03)
-    time.sleep(1)
+    # initialize display
+    screen.text="Initialisaiton"
 
-disp.module_exit()
-fan_pwm.stop()
-GPIO.cleanup()
+    button = Button()
+    button.start()
+
+    hyperHDR = HyperHDR()
+    hyperHDR.start()
+
